1530586c820b243c93c0b5fb038d8c664d45d99b
Thomas Fuller <mtfuller1995@gmail.com>
2018-03-19 23:48:32 -0400

    Added code to run the pretrained C3D Model

--- model.py
+++ model.py
@@ -2,6 +2,25 @@
 #date: 03/06/2018
 
 import tensorflow as tf
+from tensorflow.contrib import layers
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+video = cv2.VideoCapture("./vid/test.avi")
+
+ret, frame = video.read()
+st_video = np.array([frame])
+print("LOADING VIDEO...")
+count = 0
+while video.isOpened():
+    count += 1
+    ret, frame = video.read()
+    if not ret or count >= 32:
+        break
+    st_video = np.vstack((st_video, [frame]))
+print("LOADED")
+print(st_video.shape)
 
 PRETRAINED_MODEL_PATH = "./pretrained_models/c3d_ucf101_finetune_whole_iter_20000_TF.model"
 
@@ -20,17 +39,59 @@ class Model:
         eggs: An integer count of the eggs we have laid.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, batch_size = 3, **kwargs):
         self.__graph = tf.Graph()
-        self.__CLIP_LENGTH = kwargs.get('clip_length',10)
-        self.__CROP_WIDTH = kwargs.get('crop_width', 10)
-        self.__CROP_HEIGHT = kwargs.get('crop_height', 10)
+        self.__CLIP_LENGTH = kwargs.get('clip_length',32)
+        self.__CROP_WIDTH = kwargs.get('crop_width', 320)
+        self.__CROP_HEIGHT = kwargs.get('crop_height', 240)
         self.dropout_prob = kwargs.get('dropout_prob', 0.6)
         self.num_class = kwargs.get('num_class', 101)
+        self.n_step_epoch = int(9537 / batch_size)
 
         with self.__graph.as_default():
             self.inputs = tf.placeholder(tf.float32, [None, self.__CLIP_LENGTH, self.__CROP_HEIGHT, self.__CROP_WIDTH, 3])
-            self.initializer = tf.contrib.xavier_initializer()
+            self.__nn = self.inputs
+            self.initializer = layers.xavier_initializer()
+            self.global_step = tf.Variable(0, trainable=False, name="global_step")
+            self.lr = tf.train.exponential_decay(1e-4, self.global_step, int(10 * self.n_step_epoch), 1e-1, True)
+
+    def run(self):
+        config = tf.ConfigProto()
+        with tf.Session(config=config, graph=self.__graph) as sess:
+            self.__build()
+
+            softmax_logits = tf.nn.softmax(self.__nn)
+            int_label = tf.placeholder(tf.int64, [3,])
+
+            task_loss = tf.reduce_sum(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.__nn, labels=int_label))
+            acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(softmax_logits, axis=-1), int_label), tf.float32))
+            right_count = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(softmax_logits, axis=1), int_label), tf.int32))
+
+            reg_loss = layers.apply_regularization(layers.l2_regularizer(5e-4),
+                                                   tf.get_collection(tf.GraphKeys.WEIGHTS))
+            total_loss = task_loss + reg_loss
+            # train_var_list = [v for v in tf.trainable_variables() if v.name.find("conv") == -1]
+            train_op = tf.train.GradientDescentOptimizer(self.lr).minimize(
+                total_loss, global_step=self.global_step)
+            # train_op = tf.train.MomentumOptimizer(self.lr,0.9).minimize(
+            #     total_loss, global_step = self.global_step,var_list=train_var_list)
+
+            total_para = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
+            print('total_para:', total_para)  # all CDC9 :28613120  #pool5 27655936
+
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            saver = tf.train.Saver(tf.trainable_variables())
+            saver.restore(sess, PRETRAINED_MODEL_PATH)
+            print("Model Loading Done!")
+            output = sess.run(self.__nn, feed_dict={self.inputs: [st_video]})
+            print("FINISHED!!!")
+            print(output)
+            output = output.T
+            #assert output.shape == (101,10)
+            scores = np.mean(output, axis=1)
+            print(scores.argsort()[-10:][::-1])
 
     def saveModel(self):
         pass
@@ -60,6 +121,14 @@ class Model:
         self.__dropout('dropout2', self.dropout_prob)
         self.__fc('fc3', [4096, self.num_class], 'wout', 'bout', False)
 
+        # Loss Function MIL
+        # L = l(B_a, B_n) + NORM(M)
+        # l(B_a, B_n) = max(0,1-y_pred_a+y_pred_n) + SUM((y_pred_a - y_pred_n)**2) + SUM(y_pred_a)
+        #_max = tf.maximum(np.zeros(), np.ones() - y_pred_a + y_pred_n)
+        #_smooth = tf.
+        #_sparsity = tf.reduce_sum(y_pred_a)
+        # loss = _max + _smooth + _sparsity
+
     def __conv3d(self, name, dim, w_name, b_name):
         with self.__graph.as_default():
             with tf.variable_scope('var_name') as var_scope:
@@ -67,12 +136,12 @@ class Model:
                 b = tf.get_variable(name=b_name, shape=dim[-1], initializer=tf.zeros_initializer(), dtype=tf.float32)
                 tf.add_to_collection(tf.GraphKeys.WEIGHTS, W)
                 tf.add_to_collection(tf.GraphKeys.BIASES, b)
-            self.__model = tf.nn.conv3d(self.__model, W, strides=[1, 1, 1, 1, 1], padding="SAME", name=name)
-            self.__model = tf.nn.relu(tf.nn.bias_add(self.__model, b))
+            self.__nn = tf.nn.conv3d(self.__nn, W, strides=[1, 1, 1, 1, 1], padding="SAME", name=name)
+            self.__nn = tf.nn.relu(tf.nn.bias_add(self.__nn, b))
 
     def __maxpool(self, name, dim):
         with self.__graph.as_default():
-            self.__model = tf.nn.max_pool3d(self.__model, ksize=dim, strides=dim, padding="SAME", name=name)
+            self.__nn = tf.nn.max_pool3d(self.__nn, ksize=dim, strides=dim, padding="SAME", name=name)
 
     def __fc(self, name, dim, w_name, b_name, activation = True):
         with self.__graph.as_default():
@@ -83,14 +152,14 @@ class Model:
                 tf.add_to_collection(tf.GraphKeys.BIASES, b)
 
             if activation:
-                self.__model = tf.nn.bias_add(tf.matmul(self.__model, W, name=name), b)
+                self.__nn = tf.nn.bias_add(tf.matmul(self.__nn, W, name=name), b)
             else:
-                self.__model = tf.nn.relu(tf.nn.bias_add(tf.matmul(self.__model, W, name=name), b))
+                self.__nn = tf.nn.relu(tf.nn.bias_add(tf.matmul(self.__nn, W, name=name), b))
 
     def __reshape(self, dim):
-        self.__model = tf.reshape(self.__model, dim)
+        self.__nn = tf.reshape(self.__nn, dim)
 
     def __dropout(self, name, prob):
         with self.__graph.as_default():
-            self.__model = tf.nn.dropout(self.__model, prob, name=name)
+            self.__nn = tf.nn.dropout(self.__nn, prob, name=name)
 
